d59a16e2cb307e705493d81ff08ae2dc4f82dfe0
sardin311002 <sardin311002@gmail.com>
2023-05-19 12:24:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Ulangan Tengah Semester (UTS)
+
+<hr>
+Nama    : Sardin <br>
+
+NIM : 312010135 <br>
+
+Kelas : TI.20. A.1 <br>
+
+<hr>
+
+# LOGIN FORM BY FLUTTER
+
+This project focuses on UI Mobile Design. I am using Visual Studio Code and utilizing the devices plugin.
+
+Once you have prepared the necessary tools and prerequisites for the UI mobile design project, you can proceed with the following steps:
+
+- Open Visual Studio Code (VSCode) and make sure you have installed the required extensions: Dart, Flutter, and Flutter Widget.
+
+- Connect your mobile device to your computer using a USB cable. It is important to ensure that you have downloaded and installed the appropriate ADB (Android Debug Bridge) driver for your specific device. This step may vary depending on the device you are using. You can search for the specific instructions for your device online.
+
+- Put your mobile device in Debug Mode. The method to enable Debug Mode may vary depending on the device and the Android version you are using. Typically, you can enable it by going to the "Developer options" in the device settings. If you don't see the "Developer options" menu, you may need to enable it first. Again, you can find device-specific instructions online.
+
+- Once your device is connected and in Debug Mode, you can clone the repository for the UI mobile design project to your local machine. You can use Git or simply download the project as a zip file and extract it to a desired location on your computer.
+
+- After cloning the repository, open the project folder in Visual Studio Code. You can do this by selecting "File" > "Open Folder" and navigating to the location where you cloned the repository.
+
+Now you are ready to start working on the UI mobile design project using Visual Studio Code. You can utilize the Flutter and Dart extensions to write code, and the Flutter Widget extension can assist you in creating and managing the UI components for your mobile app.
+
+# Configuration for Development
+
+Duplicate the repository and execute the subsequent commands:
+
+```
+flutter pub get
+flutter run
+```
+
+# Output
+
+## Tampilan Welcome
+
+![pict](pict/ss1.png)
+
+## Tampilan Login
+
+![pict](pict/ss2.png)
+
+## Tampilan sign up
+
+![pict](pict/ss3.png)
---
+++ lib/register.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+class myRegister extends StatefulWidget {
+  const myRegister({Key? key}) : super(key: key);
+
+  @override
+  _myRegisterState createState() => _myRegisterState();
+}
+
+class _myRegisterState extends State<myRegister> {
+  //Password Field obscureText  Handler
+  bool _isHidden = true;
+  void _toggleVisibility() {
+    setState(() {
+      _isHidden = !_isHidden;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/register.png',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Scaffold(
+          appBar: AppBar(
+              elevation: null,
+              backgroundColor: Color.fromARGB(0, 249, 246, 246),
+              leading: TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'login');
+                },
+                child: Icon(
+                  Icons.arrow_back_ios_rounded,
+                  color: Colors.white,
+                ),
+              )),
+          backgroundColor: Color.fromARGB(0, 23, 1, 1),
+          body: Stack(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'REGISTER\n NOW',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                ],
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.28,
+                    left: 35,
+                    right: 35,
+                  ),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                          fillColor: Color.fromARGB(255, 246, 243, 243),
+                          filled: true,
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                              borderSide: const BorderSide(
+                                color: Colors.white,
+                              )),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(255, 235, 225, 225),
+                          prefixIcon: Icon(Icons.email_outlined),
+                          filled: true,
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextField(
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(248, 248, 243, 243),
+                          prefixIcon: Icon(Icons.phone),
+                          filled: true,
+                          labelText: 'Phone',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      TextField(
+                        obscureText: _isHidden,
+                        decoration: InputDecoration(
+                          fillColor: Color.fromARGB(242, 234, 227, 227),
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: IconButton(
+                            onPressed: _toggleVisibility,
+                            icon: _isHidden
+                                ? Icon(Icons.visibility)
+                                : Icon(Icons.visibility_off),
+                          ),
+                          filled: true,
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                maximumSize: const Size(170.0, 90.0),
+                                minimumSize: const Size(170.0, 60.0),
+                                primary: Color.fromARGB(223, 238, 234, 234),
+                                shape: const StadiumBorder(),
+                              ),
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                //crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text('REGISTER'),
+                                  Icon(
+                                    Icons.content_paste_rounded,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              )),
+                        ],
+                      ),
+                      SizedBox(height: 30.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'login');
+                            },
+                            child: Text(
+                              'Login',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 0, 4, 248)),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, 'forgot');
+                            },
+                            child: Text(
+                              'Forgot password?',
+                              style: TextStyle(
+                                  color: Color.fromARGB(228, 219, 205, 205)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
